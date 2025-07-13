e51ada0a8df7eebf52e7542be6147051126279ce
Yosef <yosefetene02@gmail.com>
2025-07-14 00:29:59 +0300

    Updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,7 @@ Redis config file
 
 import redis
 import uuid
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -18,15 +19,15 @@ class Cache:
         Initializes the Redis client and flushes the current database.
         '''
         self._redis = redis.Redis(
-            host='localhost', port=6379, decode_responses=True)
+            host='localhost', port=6379, decode_responses=False)
         self._redis.flushdb()
 
-    def store(self, data: str) -> str:
+    def store(self, data: Union[str | int | float | bytes]) -> str:
         """
         Stores the given data in Redis with a generated UUID key.
 
         Args:
-            data (str): The data to store.
+            data (str | bytes | int | float): The data to store.
 
         Returns:
             str: The generated UUID key under which the data is stored.
@@ -36,3 +37,26 @@ class Cache:
         self._redis.set(uuid_value, data)
 
         return uuid_value
+
+    def get(
+            self, key: str, fn: Optional[Callable] = None
+            ) -> Optional[Union[str, int, bytes]]:
+        """
+        Retrieve data from redis
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves string data from redis
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> str:
+        """
+        Retrieves int from redis
+        """
+        return self.get(key, fn=int)
